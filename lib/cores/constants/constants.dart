311aa61f85e592bbba2d768e4fd9fa38eee28a84
chera-mihiretu <cheramihiretucm@gmail.com>
2024-08-27 08:54:23 -0400

    Setting up the constants

---
+++ lib/cores/constants/constants.dart
@@ -0,0 +1,6 @@
+class AppConstant {
+  static const String imageUrl =
+      'https://www.foodandwine.com/thmb/jldKZBYIoXJWXodRE9ut87K8Mag=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/crispy-comte-cheesburgers-FT-RECIPE0921-6166c6552b7148e8a8561f7765ddf20b.jpg';
+  static const String baseUrl =
+      'https://g5-flutter-learning-path-be.onrender.com/api/v1/groceries';
+}
